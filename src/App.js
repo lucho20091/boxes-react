f4e3fc46a016455cee5c4126f0bedc88f8ad6fa0
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-12 12:01:36 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Box from './components/Box'
+import boxes from './boxes'
 
-function App() {
+export default function App() {
+  const [squares, setSquares] = React.useState(boxes)
+  console.log(squares)
+  
+  function toggle(id) {
+      setSquares(prevSquares => {
+          return prevSquares.map((square) => {
+              return square.id === id ? {...square, on: !square.on} : square
+          })
+      })
+  }
+  
+  const squareElements = squares.map(square => (
+      <Box 
+          key={square.id} 
+          id={square.id}
+          on={square.on} 
+          toggle={() => toggle(square.id)}
+      />
+  ))
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <main>
+          {squareElements}
+      </main>
+  )
 }
-
-export default App;
